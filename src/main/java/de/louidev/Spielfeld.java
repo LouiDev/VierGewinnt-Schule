627c81e5c7a36d32dfd6b9c99acf814d9ee0c01b
LouiDev <2705loui@gmail.com>
2026-06-09 11:03:43 +0200

    feat: add more constants to ``Spielfeld.java``

--- src/main/java/de/louidev/Spielfeld.java
+++ src/main/java/de/louidev/Spielfeld.java
@@ -1,8 +1,11 @@
 package de.louidev;
 
 public class Spielfeld {
-	private final int spielfeld[][] = new int[7][6];
+	private final int spielfeld[][] = new int[BOARD_WIDTH][BOARD_HEIGHT];
 	private int playerTurn = 0;
+
 	private static final int PLAYER_ONE = 0;
 	private static final int PLAYER_TWO = 1;
+	private static final int BOARD_HEIGHT = 6;
+	private static final int BOARD_WIDTH = 7;
 }
