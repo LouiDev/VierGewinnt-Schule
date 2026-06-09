ea7e1b50bb1278f0155e1e07c77d361909d40338
Janko Grünzig <j.gruenzig@prodware.de>
2026-06-09 11:00:54 +0200

    Added Attributes

--- src/main/java/de/louidev/Spielfeld.java
+++ src/main/java/de/louidev/Spielfeld.java
@@ -1,5 +1,8 @@
 package de.louidev;
 
 public class Spielfeld {
-
+	private final int spielfeld[][] = new int[7][6];
+	private int playerTurn = 0;
+	private static final int PLAYER_ONE = 0;
+	private static final int PLAYER_TWO = 1;
 }
